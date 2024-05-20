4916ea2124de0a8df7e9542ab3cea5082e9adb62
Connor Antony <connor.antony19@gmail.com>
2024-05-20 18:20:06 -0500

    Worked on Website

---
+++ portfolio/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-r from-blue-700 to-gray-900">
+      {/* <h1>Hello World</h1> */}
+    </main>
+  );
+}
